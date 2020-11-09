87cd8a779add93c276b1b509a56806cedf9f65ab
Joe Love <joelove100@gmail.com>
2020-11-09 07:07:20 +0000

    Add html for line chart menu component

---
+++ frontend/src/app/modules/data-viewer/components/data-viewer-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-viewer-menu',
+  templateUrl: './data-viewer-menu.component.html',
+  styleUrls: ['./data-viewer-menu.component.css']
+})
+export class DataViewerMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- frontend/src/app/modules/data-viewer/data-viewer.module.ts
+++ frontend/src/app/modules/data-viewer/data-viewer.module.ts
@@ -2,12 +2,13 @@ import {NgModule} from '@angular/core';
 import {SharedModule} from '../../shared/shared.module';
 import { DataViewerComponent } from './components/data-viewer.component';
 import { MainLineChartComponent } from './components/main-line-chart.component';
+import { DataViewerMenuComponent } from './components/data-viewer-menu.component';
 
 @NgModule({
   declarations: 
   [
     DataViewerComponent, 
-    MainLineChartComponent
+    MainLineChartComponent, DataViewerMenuComponent
   ],
   imports: 
   [
